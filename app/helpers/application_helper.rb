0d63549c7ac4b0d405ab51c7e51fbe6bdb693f05
cassandralin <cassandralin92@gmail.com>
2020-03-04 18:39:30 +0000

    added empty cart feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
 end
+def render_cart_conditionally
+  if enhanced_cart.length != 0
+    puts "----->#{enhanced_cart.length}"
+    render :partial => 'cart'
+  else
+    render :partial => 'empty_cart'
+
+  end
+end 
