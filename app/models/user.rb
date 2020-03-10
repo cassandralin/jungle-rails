f08443553d44c0694a81f66b6b103d73448b2362
cassandralin <cassandralin92@gmail.com>
2020-03-10 22:10:02 +0000

    fixed product testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :name, presence: true
 
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { in: 6..20 }, confirmation: true
+
+  validates :password_confirmation, presence: true, if: :password_changed?
 end
 
